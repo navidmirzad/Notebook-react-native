cc569fd599c4d452c3bf6126d4d6315d48288f3c
navidmirzad <navi0221@stud.kea.dk>
2024-03-24 16:12:31 +0100

    added background image for notebook

--- Authentication.js
+++ Authentication.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 import { useState } from "react";
 import axios from "axios";
+import { ImageBackground } from "react-native-web";
 
 export default function MyComponent() {
   const API_KEY = "AIzaSyBNX-p8DRubGnM8K3QgtHi_s7ccok5BaIE";
@@ -26,32 +27,75 @@ export default function MyComponent() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, React Native!</Text>
-      <TextInput
-        onChangeText={(newText) => setEnteredEmail(newText)}
-        value={enteredEmail}
-      />
-      <TextInput
-        onChangeText={(newText) => setEnteredPassword(newText)}
-        value={enteredPassword}
-      />
-      <Button title="Login" onPress={login} style={styles.button} />
-    </View>
+    <ImageBackground
+      source={require("./images/ny.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <View style={styles.div}>
+          <View>
+            <Text style={styles.text}>Register</Text>
+            <TextInput
+              onChangeText={(newText) => setEnteredEmail(newText)}
+              value={enteredEmail}
+              style={styles.input}
+            />
+            <TextInput
+              onChangeText={(newText) => setEnteredPassword(newText)}
+              value={enteredPassword}
+              style={styles.input}
+            />
+            <Button title="REGISTER" onPress={login} style={styles.button} />
+          </View>
+          <View>
+            <Text style={styles.text}>Login</Text>
+            <TextInput
+              onChangeText={(newText) => setEnteredEmail(newText)}
+              value={enteredEmail}
+              style={styles.input}
+            />
+            <TextInput
+              onChangeText={(newText) => setEnteredPassword(newText)}
+              value={enteredPassword}
+              style={styles.input}
+            />
+            <Button title="LOGIN" onPress={login} style={styles.button} />
+          </View>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
+  div: {
+    borderWidth: 2,
+    borderColor: "gray",
+    padding: 60,
+    backgroundColor: "white",
+  },
   text: {
+    textAlign: "center",
     fontSize: 24,
     marginBottom: 20,
   },
   button: {
     fontSize: 20,
+  },
+  input: {
+    fontSize: 16,
+    flex: 1,
+    textAlign: "center",
   },
 });
