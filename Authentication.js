9c87cf467722d6674f4c1d80016e637d79c76f3f
navidmirzad <navi0221@stud.kea.dk>
2024-03-24 15:27:53 +0100

    loginForm firebase

--- Authentication.js
+++ Authentication.js
@@ -1,28 +1,41 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+import { useState } from "react";
 import axios from "axios";
 
 export default function MyComponent() {
   const API_KEY = "AIzaSyBNX-p8DRubGnM8K3QgtHi_s7ccok5BaIE";
   const url =
     "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
+  const [enteredEmail, setEnteredEmail] = useState("navidmirzad@hotmail.com");
+  const [enteredPassword, setEnteredPassword] = useState("navid123");
 
   async function login() {
     try {
       const response = await axios.post(url + API_KEY, {
-        email: "navidmirzad@hotmail.com",
-        password: "navid123",
+        email: enteredEmail,
+        password: enteredPassword,
         returnSecureToken: true,
       });
-      alert("Logged in");
+      alert("Logged in" + response.data.idToken);
     } catch (error) {
-      alert("Ikke logget ind, error: " + error);
+      alert(
+        "Ikke logget ind, error: " + error.response.data.error.errors[0].message
+      );
     }
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello, React Native!</Text>
+      <TextInput
+        onChangeText={(newText) => setEnteredEmail(newText)}
+        value={enteredEmail}
+      />
+      <TextInput
+        onChangeText={(newText) => setEnteredPassword(newText)}
+        value={enteredPassword}
+      />
       <Button title="Login" onPress={login} style={styles.button} />
     </View>
   );
